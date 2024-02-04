36b69943998ca3d49fcd4a81837f694a6cfb5438
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2024-02-04 12:00:35 -0500

    changed initial screen to segue to main photobooth screen if desired status code from http response

--- photobooth/ViewController.swift
+++ photobooth/ViewController.swift
@@ -8,10 +8,9 @@
 import UIKit
 import Foundation
 
-
-
 class ViewController: UIViewController {
     
+    @IBOutlet weak var connectButton: UIButton!
     @IBOutlet weak var ipAddress: UITextField!
     @IBOutlet weak var portNumber: UITextField!
 
@@ -22,6 +21,19 @@ class ViewController: UIViewController {
     
     @IBAction func connnectToCamera(_ sender: UIButton) {
         
+        let url = URL(string: "http://" + ipAddress.text! + ":" + portNumber.text! + "/ccapi")!
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "didConnectToCamera", sender: nil)
+                    }
+                }
+            }
+        }
+        task.resume()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
