e2819341ee1c2e308f7438e136152db0f50fbf72
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2025-02-22 12:16:15 -0500

    remove printer info, change to one ip address input field

--- photobooth/ViewController.swift
+++ photobooth/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     // Outlet Variables
     @IBOutlet weak var connectButton: UIButton!
     @IBOutlet var ipAddressField: UITextField!
-    @IBOutlet var portNumberField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,15 +19,14 @@ class ViewController: UIViewController {
     
     // Connect to camera with inputted IP address and port number
     @IBAction func connnectToCamera(_ sender: UIButton) {
-        let url = URL(string: "http://" + ipAddressField.text! + ":" + portNumberField.text! + "/ccapi")!
+        let url = URL(string: "http://" + ipAddressField.text! + "/ccapi")!
         let task = URLSession.shared.dataTask(with: url) {
             data, response, error in
             
             if let response = response as? HTTPURLResponse {
                 if response.statusCode == 200 { // If HTTP response from camera is 200 (no errors)
                     DispatchQueue.main.async {
-                        ipAddress = self.ipAddressField.text! // Set global variables to inputted variables
-                        portNumber = self.portNumberField.text!
+                        cameraIP = self.ipAddressField.text! // Set global variables to inputted variables
                         self.performSegue(withIdentifier: "didConnectToCamera", sender: nil) // Move to photobooth viewcontroller
                     }
                 }
